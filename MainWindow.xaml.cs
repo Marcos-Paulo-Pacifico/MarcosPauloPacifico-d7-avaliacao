c57a6ac4e79abf41ce6138489345cf7ddd825019
Paulocas <marcospacifico@usp.br>
2022-11-30 15:34:48 -0300

    Adiciona condicao de login

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,17 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MarcosPauloPacifico_d7_avaliacao
 {
@@ -35,8 +26,20 @@ namespace MarcosPauloPacifico_d7_avaliacao
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var oi = context.Users.ToList();
-            
 
+
+            foreach(var user in oi)
+            {
+                if (user.Email == Email.Text)
+                {
+                    if (user.Password == Password.Text)
+                    {
+                        MessageBox.Show("Usuário logado com êxito!");
+                    }
+                    else MessageBox.Show("Senha Inválida");
+                }
+                else MessageBox.Show("Usuário não encontrado");
+            }
             
         }
     }
